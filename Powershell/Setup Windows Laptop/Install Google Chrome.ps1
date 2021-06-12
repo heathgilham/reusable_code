7a1cfd464e3ed6e551a9cc25a28a9193e73352d5
Heath <heathgilham@gmail.com>
2021-06-12 17:06:13 +1000

    Install Google Chrome on Windows laptop

---
+++ Powershell/Setup Windows Laptop/Install Google Chrome.ps1
@@ -0,0 +1,5 @@
+## Purpose: Install Google Chrome automatically
+## Author: Heath Gilham
+## Instructions: 1. Open Powershell session in Admin mode
+##               2. Copy and run this script
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
